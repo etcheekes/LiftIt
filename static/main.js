17b320afaf25fb6883e2ed3d333e79d9e686167b
etcheekes <echeekes@gmail.com>
2022-12-20 15:08:43 +0000

    function to change header text

--- static/main.js
+++ static/main.js
@@ -4,4 +4,10 @@ function showField() {
     document.getElementById('hiddenField').hidden = false;
   }
 
-  
+// change content in header
+function changeHeader(h_tag, order, change) {
+  // reference the header element
+  let header = document.getElementsByTagName(h_tag)[order];
+  // change its content
+  header.innerHTML = change;
+}
