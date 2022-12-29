a5371b6a686dde7e3e863c8be113df806068977d
etcheekes <echeekes@gmail.com>
2022-12-29 21:14:51 +0000

    function to reveal form in elements of a certain class

--- static/main.js
+++ static/main.js
@@ -30,3 +30,22 @@ function submitForm(id) {
 function conDelete() {
   return confirm("Confirm removal")
 }
+
+// reveal form by class
+
+function revealClassForm(className) {
+  // obtain all elements with the class access_reps
+  getElements = document.getElementsByClassName(className);
+          
+  // add event listener to each element with this class and reveal form tag upon clicking
+  for (let i = 0; i < getElements.length; i++) {
+      getElements[i].addEventListener("click", function() {
+          const td = this.parentNode; // Get the parent <td> element of the button
+          const forms = td.getElementsByTagName("form"); // Get the <form> elements in the <td> element
+          if (forms.length > 0) {  // Make sure there is at least one form element
+              forms[0].style.display = "block";  // Set the display property of the first form element to "block"
+          } 
+      });
+      }
+  }
+
